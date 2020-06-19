6e629615697abd4cb771c8e9d4d3a373c24f206f
Jj <jminneyiii@gmail.com>
2020-06-18 21:24:32 -0600

    Added GridView and Pairs of Names and URLs I'm using Picasso to grab the pictures. Down on the row with 'God's Not Dead' and "TPOBWF" there is a bug there. I'll have to find a way to fix that later.

--- Week-4/MovieApp/app/src/main/java/com/minneydev/movieapp/MainActivity.kt
+++ Week-4/MovieApp/app/src/main/java/com/minneydev/movieapp/MainActivity.kt
@@ -2,16 +2,21 @@ package com.minneydev.movieapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.GridLayoutManager
 import com.minneydev.movieapp.ui.MainAdapter
 import kotlinx.android.synthetic.main.activity_main.*
 
+//A few of the movies Jen put in hers are some of my favorites lol. I promise I didn't cheat
+
 class MainActivity : AppCompatActivity() {
+
+    private val spanCount = 2
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        mainRecyclerView.layoutManager = LinearLayoutManager(this)
+        mainRecyclerView.layoutManager = GridLayoutManager(this, spanCount)
         mainRecyclerView.adapter = MainAdapter()
 
     }
